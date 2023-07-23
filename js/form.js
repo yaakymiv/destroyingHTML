36e161a7ad34a680e9b5a54ab4090e6edac4c3fa
yaakymiv <yaryna.yakymiv.shi.2022@lpnu.ua>
2023-07-23 18:35:50 +0300

    more complex form for demo

--- js/form.js
+++ js/form.js
@@ -11,68 +11,94 @@ const closeButton1 = document.getElementById("close_btn1");
 const closeButton2 = document.getElementById("close_btn2");
 const overlay = document.getElementById("overlay");
 
-
 closeButton1.addEventListener("click", () => {
   popup1.style.display = "none";
   overlay.style.display = "none";
+  e_name.innerHTML="";
+  e_phone.innerHTML="";
+  e_email.innerHTML="";
   document.getElementById("order_form").reset();
-  document.getElementById('e-name').style.display="none";
-  document.getElementById('e-phone').style.display="none";
-  document.getElementById('e-email').style.display="none";
+});
+
+
+
+var e_name = document.getElementById("e-name");
+function validateName() {
+  var name = document.getElementById("name").value;
+  if (name.length<=2) {
+    e_name.innerHTML = "Введіть більше двох літер";
+    return false;
+  }
+  e_name.innerHTML =
+    '<i class="fa-solid fa-circle-check" style="color: #4aed7b; position:relative;top:-30px;right:10px;"></i>';
+    return true;
+}
+
+var e_phone = document.getElementById("e-phone");
+function validatePhone() {
+  var phone = document.getElementById("phone").value;
+  if (phone.length === 0) {
+    e_phone.innerHTML = ""; 
+    return true; 
+  }
+  if (phone.length!=9||!phone.match(/^[0-9]{9}$/)) {
+    e_phone.innerHTML = "Номер має містити 9 цифр";
+    return false;
+  }
+  e_phone.innerHTML =
+  '<i class="fa-solid fa-circle-check" style="color: #4aed7b; position:relative;top:-30px;right:10px;"></i>';
+    return true;
+}
+
+var e_email = document.getElementById("e-email");
+function validateEmail() {
+  var email = document.getElementById("email").value;
+  if (!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)) {
+    e_email.innerHTML = "Дотримуйтесь формату: example@user.com";
+    return false;
+  }
+  e_email.innerHTML =
+  '<i class="fa-solid fa-circle-check" style="color: #4aed7b; position:relative;top:-30px;right:10px;"></i>';
+    return true;
+}
+
+
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const emailInput = document.getElementById("email");
+
+nameInput.addEventListener("input", updateButtonColor);
+phoneInput.addEventListener("input", updateButtonColor);
+emailInput.addEventListener("input", updateButtonColor);
+
+function updateButtonColor() {
+  if (validateName() && validatePhone() && validateEmail()) {
+    orderButton.classList.add("button-success");
+  } else {
+    orderButton.classList.remove("button-success");
+  }
+}
+
+const popup2 = document.getElementById("popup2");
+orderButton = document.getElementById("order_btn");
+
+orderButton.addEventListener("click", () => {
+  if(!validateName() || !validatePhone() || !validateEmail()){
+    return;
+  }else{
+    orderButton.classList.remove("button-success");
+    e_name.innerHTML="";
+    e_phone.innerHTML="";
+    e_email.innerHTML="";
+    popup1.style.display = "none";
+    popup2.style.display = "block";
+  }
 });
 
 closeButton2.addEventListener("click", () => {
   popup2.style.display = "none";
   overlay.style.display = "none";
   document.getElementById("order_form").reset();
-});
-
-
-orderButton = document.getElementById('order_btn');
-const popup2 = document.getElementById("popup2");
-
-function validateName() {
-  var name = document.getElementById("name_form").value;
-  return name.length>1;
-}
-
-function validatePhone() {
-  var phone = document.getElementById("phone").value;
-  return phone.length == 9 && phone.match(/^[0-9]{9}$/);
-}
-
-function validateEmail() {
-  var email = document.getElementById("email").value;
-  var emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-  return emailPattern.test(email);
-}
-
-
-orderButton.addEventListener("click", () => {
-  if(validateName()&&validatePhone()&&validateEmail()){
-    popup1.style.display = "none";
-    popup2.style.display = "block";
-    document.getElementById('e-name').style.display="none";
-    document.getElementById('e-phone').style.display="none";
-    document.getElementById('e-email').style.display="none";
-  }else{
-    if(!validateName()){
-      document.getElementById('e-name').style.display="block";
-    }else{
-      document.getElementById('e-name').style.display="none";
-    }
-    if(!validatePhone()){
-      document.getElementById('e-phone').style.display="block";
-    }else{
-      document.getElementById('e-phone').style.display="none";
-    }
-
-    if(!validateEmail()){
-      document.getElementById('e-email').style.display="block";
-    }else{
-      document.getElementById('e-email').style.display="none";
-    }
-  }
 });
 
 
